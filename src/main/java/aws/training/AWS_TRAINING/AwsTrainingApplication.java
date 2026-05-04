9bf79062de459127a89c474d802e0172b81897a5
pedrohenauth2 <pedrohenauth1212@gmail.com>
2026-05-04 09:19:39 -0300

    First commit

---
+++ src/main/java/aws/training/AWS_TRAINING/AwsTrainingApplication.java
@@ -0,0 +1,13 @@
+package aws.training.AWS_TRAINING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aws/training/AWS_TRAINING/AwsTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.training.AWS_TRAINING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
