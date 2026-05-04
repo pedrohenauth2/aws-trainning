ba78f34b36f8ab2e2ac3046a3e4db24d5c7a9a12
Pedro Henauth <pedrohenauth1212@gmail.com>
2026-05-04 11:26:58 -0300

    CMT-02: Adicionando localstack AWS para testar serviços AWS locais

---
+++ src/main/java/aws/training/AWS_TRAINING/config/AwsLocalStackConfig.java
@@ -0,0 +1,107 @@
+package aws.training.AWS_TRAINING.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.ssm.SsmClient;
+
+import java.net.URI;
+
+@Configuration
+public class AwsLocalStackConfig {
+
+    @Value("${aws.localstack.endpoint}")
+    private String endpoint;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${aws.credentials.secret-key}")
+    private String secretKey;
+
+    private StaticCredentialsProvider credentialsProvider() {
+        return StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey)
+        );
+    }
+
+    private Region awsRegion() {
+        return Region.of(region);
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(awsRegion())
+                .credentialsProvider(credentialsProvider())
+                .forcePathStyle(true) // necessário para LocalStack
+                .build();
+    }
+
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(awsRegion())
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public SnsClient snsClient() {
+        return SnsClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(awsRegion())
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(awsRegion())
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public LambdaClient lambdaClient() {
+        return LambdaClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(awsRegion())
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public SecretsManagerClient secretsManagerClient() {
+        return SecretsManagerClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(awsRegion())
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public SsmClient ssmClient() {
+        return SsmClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(awsRegion())
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+}
